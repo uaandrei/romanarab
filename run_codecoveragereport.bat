41892da636d6870d3510ad183ac93d7ccefbe005
Andrei Ursta <andrei.ursta@canamgroupinc.com>
2014-07-17 12:55:31 +0300

    ignored test_report files

--- run_codecoveragereport.bat
+++ run_codecoveragereport.bat
@@ -1,7 +1,7 @@
-set dll="RomanNumbersCalculator.Tests\bin\Debug\RomanNumbersCalculator.Tests.dll"
+set dll="/testcontainer:RomanNumbersCalculator.Tests\bin\Debug\RomanNumbersCalculator.Tests.dll /noisolation"
 set opencover_exe="packages\OpenCover.4.5.2506\OpenCover.Console.exe"
 set reportgenerator_exe="packages\ReportGenerator.1.9.1.0\ReportGenerator.exe"
-set test_exe="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+set test_exe="C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\MSTest.exe"
 
 set opencoverfile="test_report\opencover.xml"
 set reportfolder="test_report\codecoverage_report"
