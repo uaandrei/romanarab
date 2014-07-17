dc4ab33364819159892b2f087e9c8cf0b284009e
andrei.ursta <ua.andrei@gmail.com>
2014-07-18 00:34:06 +0300

    implemented roman unit parser

---
+++ RomanNumbersCalculator.BL/RomanUnitsParser.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace RomanNumbersCalculator.BL
+{
+    public class RomanUnitsParser
+    {
+        private List<string> _romanUnitsList;
+
+        public RomanUnitsParser()
+        {
+            var romanNumbersGenerator = new RomanNumbersGenerator();
+            _romanUnitsList = romanNumbersGenerator.GenerateRomanNumbers(new RomanSymbolsGroup("I", "V", "X"));
+            _romanUnitsList.Reverse();
+        }
+
+        public List<string> Parse(string romanString)
+        {
+            var listOfUnits = new List<string>();
+            var hasFoundRomanUnit = false;
+            var indexOfFirstRomanUnitInRomanString = int.MaxValue;
+            var foundRomanUnit = string.Empty;
+
+            do
+            {
+                hasFoundRomanUnit = false;
+                foreach (var romanUnit in _romanUnitsList)
+                {
+                    if (romanString.Contains(romanUnit))
+                    {
+                        var indexOfCurrentRomanUnitInRomanString = romanString.IndexOf(romanUnit);
+                        if (indexOfCurrentRomanUnitInRomanString < indexOfFirstRomanUnitInRomanString)
+                        {
+                            indexOfFirstRomanUnitInRomanString = indexOfCurrentRomanUnitInRomanString;
+                            foundRomanUnit = romanUnit;
+                        }
+                        hasFoundRomanUnit = true;
+                    }
+                }
+                if (hasFoundRomanUnit)
+                {
+                    romanString = romanString.Remove(indexOfFirstRomanUnitInRomanString, foundRomanUnit.Length);
+                    listOfUnits.Add(foundRomanUnit);
+                }
+                indexOfFirstRomanUnitInRomanString = int.MaxValue;
+
+            } while (hasFoundRomanUnit);
+
+            return listOfUnits;
+        }
+    }
+}
