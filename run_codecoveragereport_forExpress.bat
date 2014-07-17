f8f220ed10bfd56081fe9e63a53bd37acc28bbf8
Andrei Ursta <andrei.ursta@canamgroupinc.com>
2014-07-17 12:53:16 +0300

    added codecoverage for mstest

---
+++ run_codecoveragereport_forExpress.bat
@@ -0,0 +1,11 @@
+set dll="RomanNumbersCalculator.Tests\bin\Debug\RomanNumbersCalculator.Tests.dll"
+set opencover_exe="packages\OpenCover.4.5.2506\OpenCover.Console.exe"
+set reportgenerator_exe="packages\ReportGenerator.1.9.1.0\ReportGenerator.exe"
+set test_exe="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+
+set opencoverfile="test_report\opencover.xml"
+set reportfolder="test_report\codecoverage_report"
+
+%opencover_exe% -register:user -target:%test_exe% -targetargs:%dll% -filter:"+[*BL*]*" -output:%opencoverfile%
+%reportgenerator_exe% -reports:%opencoverfile% -targetdir:%reportfolder%
+call %reportfolder%"\index.htm"
