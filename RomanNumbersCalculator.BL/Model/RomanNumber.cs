5586e501bbb7912ddec6f07b57cd1f60deffe9df
andrei.ursta <ua.andrei@gmail.com>
2014-07-19 01:04:12 +0300

    added validation for RomanNumber

--- RomanNumbersCalculator.BL/Model/RomanNumber.cs
+++ RomanNumbersCalculator.BL/Model/RomanNumber.cs
@@ -1,8 +1,14 @@
-﻿namespace RomanNumbersCalculator.BL.Model
+﻿using RomanNumbersCalculator.BL.RomanNumberSpecification;
+using System;
+using System.ComponentModel;
+
+namespace RomanNumbersCalculator.BL.Model
 {
-    public class RomanNumber : NotifiableObject
+    public class RomanNumber : NotifiableObject, IDataErrorInfo
     {
         private string _value;
+        private string _errorString;
+        private RomanNumberValidator _validator;
 
         public string Value
         {
@@ -17,6 +23,26 @@
         public RomanNumber()
         {
             _value = string.Empty;
+            _validator = new RomanNumberValidator(new ConsecutiveRomanPositionalsMustBeDescendingAndUnique());
+        }
+
+        public string Error
+        {
+            get { return _errorString; }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case "Value":
+                        _errorString = _validator.IsValid(_value) ? string.Empty : "Invalid roman number";
+                        break;
+                }
+                return _errorString;
+            }
         }
     }
 }
