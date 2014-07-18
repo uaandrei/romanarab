aa9b8002caa80d73543794a36c0a3f4842e85b15
andrei.ursta <ua.andrei@gmail.com>
2014-07-18 20:45:49 +0300

    implemented RomanNumberCalculator

--- RomanNumbersCalculator.BL/Calculator/RomanNumberCalculator.cs
+++ RomanNumbersCalculator.BL/Calculator/RomanNumberCalculator.cs
@@ -1,5 +1,8 @@
-﻿using RomanNumbersCalculator.BL.RomanNumberExceptions;
+﻿using System;
+using System.Linq;
+using RomanNumbersCalculator.BL.RomanNumberExceptions;
 using RomanNumbersCalculator.BL.RomanNumberSpecification;
+using System.Collections.Generic;
 
 namespace RomanNumbersCalculator.BL.Calculator
 {
@@ -8,12 +11,31 @@ namespace RomanNumbersCalculator.BL.Calculator
         private RomanNumberValidator _numberValidator;
         private RomanStringParser _parser;
         private RomanPositionalIdentifier _identifier;
+        private StringBasedCalculator _unitsCalculator;
+        private StringBasedCalculator _tensCalculator;
+        private StringBasedCalculator _hundredsCalculator;
+        private StringBasedCalculator _thousandsCalculator;
 
         public RomanNumberCalculator()
         {
             _numberValidator = new RomanNumberValidator(new ConsecutiveRomanPositionalsMustBeDescendingAndUnique());
             _parser = new RomanStringParser();
             _identifier = new RomanPositionalIdentifier();
+            var symbols = new List<string>() { null };
+            symbols.AddRange(RomanNumbersContainer.Instance.Units);
+            _unitsCalculator = new StringBasedCalculator(symbols);
+
+            symbols = new List<string>() { null };
+            symbols.AddRange(RomanNumbersContainer.Instance.Tens);
+            _tensCalculator = new StringBasedCalculator(symbols);
+
+            symbols = new List<string>() { null };
+            symbols.AddRange(RomanNumbersContainer.Instance.Hundreds);
+            _hundredsCalculator = new StringBasedCalculator(symbols);
+
+            symbols = new List<string>() { null };
+            symbols.AddRange(RomanNumbersContainer.Instance.Thousands);
+            _thousandsCalculator = new StringBasedCalculator(symbols);
         }
 
         public string Add(string number1, string number2)
@@ -27,7 +49,36 @@ namespace RomanNumbersCalculator.BL.Calculator
                 throw new InvalidRomanNumberException("Second number is invalid.");
             }
 
-            var positionalNumbersForFirstNumber = _parser.Parse(number1); return "";
+            var positionalNumbersForFirstNumber = _parser.Parse(number1);
+            var positionalNumbersForSecondNumber = _parser.Parse(number2);
+            var additionResult = string.Empty;
+            bool hasTransport = false;
+
+            var value1 = positionalNumbersForFirstNumber.FirstOrDefault(p => _identifier.IsFromUnits(p));
+            var value2 = positionalNumbersForSecondNumber.FirstOrDefault(p => _identifier.IsFromUnits(p));
+            var unitsResult = _unitsCalculator.Add(value1, value2, ref hasTransport);
+            additionResult = unitsResult;
+
+            value1 = positionalNumbersForFirstNumber.FirstOrDefault(p => _identifier.IsFromTens(p));
+            value2 = positionalNumbersForSecondNumber.FirstOrDefault(p => _identifier.IsFromTens(p));
+            var tensResult = _tensCalculator.Add(value1, value2, ref hasTransport);
+            additionResult = tensResult + additionResult;
+
+            value1 = positionalNumbersForFirstNumber.FirstOrDefault(p => _identifier.IsFromHundreds(p));
+            value2 = positionalNumbersForSecondNumber.FirstOrDefault(p => _identifier.IsFromHundreds(p));
+            var hundredsResult = _hundredsCalculator.Add(value1, value2, ref hasTransport);
+            additionResult = hundredsResult + additionResult;
+
+            value1 = positionalNumbersForFirstNumber.FirstOrDefault(p => _identifier.IsFromThousands(p));
+            value2 = positionalNumbersForSecondNumber.FirstOrDefault(p => _identifier.IsFromThousands(p));
+            var thousandsResult = _thousandsCalculator.Add(value1, value2, ref hasTransport);
+            if (hasTransport)
+            {
+                throw new OverflowException("Result is too big");
+            }
+            additionResult = thousandsResult + additionResult;
+
+            return additionResult;
         }
     }
 }
--- RomanNumbersCalculator.Tests/RomanNumberCalculatorTests.cs
+++ RomanNumbersCalculator.Tests/RomanNumberCalculatorTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RomanNumbersCalculator.BL.Calculator;
+using RomanNumbersCalculator.BL.RomanNumberExceptions;
 
 namespace RomanNumbersCalculator.Tests
 {
@@ -22,19 +23,54 @@ namespace RomanNumbersCalculator.Tests
             var number2 = "CDLXXXIX";
 
             var actual = _romanNumberCalculator.Add(number1, number2);
-            var expected = "MMLIII";
+            var expected = "MMXXXIII";
 
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void Add_ThrowsException_When_NumbersAreInIncorectFormat()
+        public void Add_ReturnsCorectValue2()
         {
+            var number1 = "MXL";
+            var number2 = "CDIV";
+
+            var actual = _romanNumberCalculator.Add(number1, number2);
+            var expected = "MCDXLIV";
+
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(InvalidRomanNumberException))]
+        public void Add_ThrowsException_When_FirstNumberHasIncorectFormat()
+        {
+            var number1 = "MXDXLIV";
+            var number2 = "CDLXXXIX";
+
+            var actual = _romanNumberCalculator.Add(number1, number2);
+            Assert.Fail("This test should fail when first number has incorect format");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidRomanNumberException))]
+        public void Add_ThrowsException_When_SecondNumberHasIncorectFormat()
+        {
+            var number1 = "MDXLIV";
+            var number2 = "CDLXXXIXV";
+
+            var actual = _romanNumberCalculator.Add(number1, number2);
+            Assert.Fail("This test should fail when second number has incorect format");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
         public void Add_ThrowsException_When_ResultIsTooLarge()
         {
+            var number1 = "MMD";
+            var number2 = "MD";
+
+            var actual = _romanNumberCalculator.Add(number1, number2);
+            Assert.Fail("This test should fail when second number has incorect format");
         }
     }
 }
